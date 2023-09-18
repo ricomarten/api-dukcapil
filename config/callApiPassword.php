aa4ccbeea853f9dd002812568a516ce6aa75876c
Rico Martenstyaro <ricomarten@gmail.com>
2023-09-18 09:03:52 +0700

    password

--- config/callApiPassword.php
+++ config/callApiPassword.php
@@ -11,10 +11,10 @@ function ekstrak_nilai($string){
 }
 $data_arr= array(
     "email"=> "ricomarten@gmail.com",
-    "password"=> "12345"
+    "password"=> "123456"
 );
 
-$url = 'localhost/api-dukcapil/services/login.php';
+$url = 'http://localhost/api-dukcapil/services/login.php';
 $options = array(
   'http' => array(
     'header'  => "Content-type: application/json,Accept: application/json\r\n",
@@ -32,39 +32,8 @@ $result = file_get_contents($url, false, $context);
 
 $hasil=json_decode($result);
 
-$content=json_decode(json_encode($hasil->content[0]), true);
-print_r($content);
-if (array_key_exists("RESPONSE_CODE",$content)){
-  //"Key exists!";
-  $status=$content['RESPONSE_CODE'];
+//$content=json_decode(json_encode($hasil), true);
+print_r($result);
+//print_r($hasil->accessToken);
 
-}
-else{
-  // "Key does not exist!";
-  $status="NIK Sesuai";
-  /*
-  "NO_KK": "Sesuai",
-      "NAMA_LGKP": "Sesuai (100)",
-      "TMPT_LHR": "Sesuai (100)",
-      "TGL_LHR": "Sesuai",
-      "PROP_NAME": "Tidak Sesuai",
-      "KAB_NAME": "Tidak Sesuai",
-      "KEC_NAME": "Tidak Sesuai",
-      "KEL_NAME": "Tidak Sesuai",
-      "NO_RT": "Tidak Sesuai",
-      "NO_RW": "Tidak Sesuai",
-      "ALAMAT": "Tidak Sesuai (0)",
-      "JENIS_PKRJN": "Tidak Sesuai",
-      "JENIS_KLMIN": "Tidak Sesuai",
-      "NO_PROP": "Tidak Sesuai",
-      "NO_KAB": "Tidak Sesuai",
-      "NO_KEC": "Tidak Sesuai",
-      "NO_KEL": "Tidak Sesuai",
-      "NIK": "Sesuai"
-      */
-      //echo  $content['NAMA_LGKP'];                              
-      echo ekstrak_nilai($content['NAMA_LGKP']);
-
-}
-echo $status;
 ?>
